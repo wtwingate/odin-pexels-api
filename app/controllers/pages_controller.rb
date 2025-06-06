58d03dc3856af4c48dee66716b1c0c121f0837a3
William Wingate <wtwingate@gmail.com>
2025-06-06 15:59:41 -0400

    Make basic version of photo search and display

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,13 @@
 class PagesController < ApplicationController
   def home
+    client = Pexels::Client.new
+
+    if params[:query].present?
+      @query = params[:query]
+      @results = client.photos.search(@query)
+      @photo_urls = @results.photos.map do |photo|
+        photo.src["medium"]
+      end
+    end
   end
 end
